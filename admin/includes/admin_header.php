2ab7c3b38f65f8b321c3fe9c7d68afbd4aa6130c
chris3evans <chris3evans.ce@gmail.com>
2023-07-17 21:48:22 +0100

    [Lesson 159]

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,6 +1,14 @@
 <?php ob_start(); ?>
 <?php session_start();?>
 <?php include "../includes/db.php"; ?>
+
+<?php
+    if (isset($_SESSION['role'])) {
+        if ($_SESSION['role'] !== 'admin') {
+            header("Location: ../index.php");
+        }
+    }
+?>
 <!DOCTYPE html>
 <html lang="en">
 
