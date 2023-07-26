c69843e8a539b91f34bf14637ff5698f7cb824db
chris3evans <chris3evans.ce@gmail.com>
2023-07-26 18:07:49 +0100

    [Lesson 185]

--- includes/view_all_posts.php
+++ includes/view_all_posts.php
@@ -1,7 +1,25 @@
 <?php
-  if (isset($_POST['checkBoxArray'])) {
-    foreach($_POST['checkBoxArray'] as $checkBoxValue) {
+  if (isset($_POST['postIdArray'])) {
+    foreach($_POST['postIdArray'] as $postIdValue) {
       $bulk_options = $_POST['bulk_options'];
+
+      switch($bulk_options) {
+        case 'published':
+          $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postIdValue}";
+          $update_status_published = mysqli_query($db_connection, $query);
+          check_query($update_status_published);
+          break;
+        case 'draft':
+          $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postIdValue}";
+          $update_status_draft = mysqli_query($db_connection, $query);
+          check_query($update_status_draft);
+          break;
+        case 'delete':
+          $query = "UPDATE posts SET post_status = '{$bulk_options}' WHERE post_id = {$postIdValue}";
+          $update_status_delete = mysqli_query($db_connection, $query);
+          check_query($update_status_delete);
+          break;
+      }
     }
   }
 ?>
@@ -11,9 +29,9 @@
     <div id="bulkOptionContainer" class="col-xs-4">
       <select class="form-control" name="bulk_options" id="">
         <option value="">Select Options</option>
-        <option value="">Publish</option>
-        <option value="">Draft</option>
-        <option value="">Delete</option>
+        <option value="published">Publish</option>
+        <option value="draft">Draft</option>
+        <option value="delete">Delete</option>
       </select>
     </div>
     <div class="col-xs-4">
@@ -60,7 +78,7 @@
       ?>
         <tr>
           <td>
-            <input class="checkBoxes" type="checkbox" name="checkBoxArray[]" value="<?php echo $post_id;?>">
+            <input class="checkBoxes" type="checkbox" name="postIdArray[]" value="<?php echo $post_id;?>">
           </td>
           <td><?php echo $post_id;?></td>
           <td><?php echo $post_author;?></td>
