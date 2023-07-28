84c9223c4647c788c7ae6e397130c792b52e772f
chris3evans <chris3evans.ce@gmail.com>
2023-07-28 21:23:16 +0100

    [Lesson 188]

--- admin/includes/add_post.php
+++ admin/includes/add_post.php
@@ -66,8 +66,11 @@
   </div>
 
   <div class="form-group">
-    <label for="post_status">Post Status</label>
-    <input type="text" class="form-control" name="post_status" />
+    <select name="post_status" id="">
+      <option value="draft">Post Status</option>
+      <option value="published">Publish</option>
+      <option value="draft">Draft</option>
+    </select>
   </div>
 
   <div class="form-group">
