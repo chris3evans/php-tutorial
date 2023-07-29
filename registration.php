a30e11b3a43a05ec2d8efa8df33d1f72fd23489a
chris3evans <chris3evans.ce@gmail.com>
2023-07-29 12:36:45 +0100

    [Lesson 200]

--- registration.php
+++ registration.php
@@ -6,33 +6,42 @@
         $registration_email = $_POST['email'];
         $registration_password = $_POST['password'];
 
-        $registration_username = mysqli_escape_string($db_connection, $registration_username);
-        $registration_email = mysqli_escape_string($db_connection, $registration_email);
-        $registration_password = mysqli_escape_string($db_connection, $registration_password);
+        if (!empty($registration_username) &&
+            !empty($registration_email) &&
+            !empty($registration_password)) {
 
-        $query = "SELECT rand_salt FROM users";
-        $select_salt_query = mysqli_query($db_connection, $query);
+            $registration_username = mysqli_escape_string($db_connection, $registration_username);
+            $registration_email = mysqli_escape_string($db_connection, $registration_email);
+            $registration_password = mysqli_escape_string($db_connection, $registration_password);
 
-        if (!$select_salt_query) {
-            die("Failed to fetch salt" . mysqli_error($db_connection));
-        }
+            $query = "SELECT rand_salt FROM users";
+            $select_salt_query = mysqli_query($db_connection, $query);
 
-        $row = mysqli_fetch_assoc($select_salt_query);
-        $salt = $row['rand_salt'];
+            if (!$select_salt_query) {
+                die("Failed to fetch salt" . mysqli_error($db_connection));
+            }
 
-        $query = "INSERT INTO users (
-            user_name,
-            user_email,
-            user_password,
-            user_role) VALUES (
-            '{$registration_username}',
-            '{$registration_email}',
-            '{$registration_password}',
-            'subscriber')";
-        $create_user_query = mysqli_query($db_connection, $query);
+            $row = mysqli_fetch_assoc($select_salt_query);
+            $salt = $row['rand_salt'];
 
-        if (!$create_user_query) {
-            die("Failed to create new user" . mysqli_error($db_connection));
+            $query = "INSERT INTO users (
+                user_name,
+                user_email,
+                user_password,
+                user_role) VALUES (
+                '{$registration_username}',
+                '{$registration_email}',
+                '{$registration_password}',
+                'subscriber')";
+            $create_user_query = mysqli_query($db_connection, $query);
+
+            if (!$create_user_query) {
+                die("Failed to create new user" . mysqli_error($db_connection));
+            }
+            $message = "Registration has been submitted";
+
+        } else {
+            $message = "Fields cannot be empty";
         }
     }
 ?>
@@ -53,6 +62,9 @@
                 <div class="form-wrap">
                 <h1>Register</h1>
                     <form role="form" action="registration.php" method="post" id="login-form" autocomplete="off">
+                        <h6 class="text-center">
+                            <?php if(!empty($message)) { echo $message; }?>
+                        </h6>
                         <div class="form-group">
                             <label for="username" class="sr-only">username</label>
                             <input type="text" name="username" id="username" class="form-control" placeholder="Enter Desired Username">
@@ -68,7 +80,6 @@
 
                         <input type="submit" name="submit" id="btn-login" class="btn btn-custom btn-lg btn-block" value="Register">
                     </form>
-
                 </div>
             </div> <!-- /.col-xs-12 -->
         </div> <!-- /.row -->
