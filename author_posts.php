c3da039e4dd8c164ae9486c3f5403851973a446b
chris3evans <chris3evans.ce@gmail.com>
2023-07-30 10:52:25 +0100

    [Lesson 210]

--- author_posts.php
+++ author_posts.php
@@ -15,26 +15,26 @@
                         <small>Secondary Text</small>
                     </h1>
                 <?php
-                    if (isset($_GET['p_id'])) {
-                        $post_id = $_GET['p_id'];
+                    if (isset($_GET['author'])) {
+                      $post_id = $_GET['p_id'];
+                      $the_post_author = $_GET['author'];
 
+                      $query = "SELECT * FROM posts WHERE post_author = '{$the_post_author}'";
+                      $all_author_posts = mysqli_query($db_connection, $query);
 
-                    $query = "SELECT * FROM posts WHERE post_id = '{$post_id}'";
-                    $all_posts = mysqli_query($db_connection, $query);
-
-                    while ($row = mysqli_fetch_assoc($all_posts)) {
-                        $post_title = $row['post_title'];
-                        $post_author = $row['post_author'];
-                        $post_date = $row['post_date'];
-                        $post_image = $row['post_image'];
-                        $post_content = $row['post_content'];
-                        $post_tags = $row['post_tags'];
+                      while ($row = mysqli_fetch_assoc($all_author_posts)) {
+                          $post_title = $row['post_title'];
+                          $post_author = $row['post_author'];
+                          $post_date = $row['post_date'];
+                          $post_image = $row['post_image'];
+                          $post_content = $row['post_content'];
+                          $post_tags = $row['post_tags'];
                 ?>
                     <h2>
                         <a href='#'><?php echo $post_title;?></a>
                     </h2>
                     <p class='lead'>
-                        by <a href='index.php'><?php echo $post_author;?></a>
+                        All Posts By <?php echo $post_author;?>
                     </p>
                     <p><span class='glyphicon glyphicon-time'></span> Posted on <?php echo $post_date;?></p>
                     <hr>
@@ -43,96 +43,7 @@
                     <p><?php echo $post_content;?></p>
                <?php }} ?>
                 <hr>
-
-                <?php
-                    if (isset($_POST['create_comment'])) {
-                        $post_id = $_GET['p_id'];
-                        $comment_author = $_POST['comment_author'];
-                        $comment_email = $_POST['comment_email'];
-                        $comment_content = $_POST['comment_content'];
-
-                        if (!empty($comment_author) &&
-                            !empty($comment_email) &&
-                            !empty($comment_content)) {
-                            $query = "INSERT INTO comments (
-                                comment_post_id,
-                                comment_author,
-                                comment_email,
-                                comment_content,
-                                comment_status,
-                                comment_date
-                                ) VALUES (
-                                $post_id,
-                                '{$comment_author}',
-                                '{$comment_email}',
-                                '{$comment_content}',
-                                'unapproved',
-                                now()
-                            )";
-                            $add_comment_query = mysqli_query($db_connection, $query);
-
-                            if (!$add_comment_query) {
-                                die("failed to add comment to post");
-                            }
-
-                            $query = "UPDATE posts SET post_comment_count = post_comment_count + 1 WHERE post_id = $post_id";
-
-                            $update_comment_count_query = mysqli_query($db_connection, $query);
-                        } else {
-                            echo "<script>alert('Fields cannot be empty')</script>";
-                        }
-                    }
-                ?>
-
-                <!-- Comments Form -->
-                <div class="well">
-                    <h4>Leave a Comment:</h4>
-                    <form role="form" method="post" action ="">
-                        <div class="form-group">
-                            <label for="comment_author">Author</label>
-                            <input type="text" name="comment_author" class="form-control"/>
-                        </div>
-                        <div class="form-group">
-                            <label for="comment_email">Email</label>
-                            <input type="email" name="comment_email" class="form-control" />
-                        </div>
-                        <div class="form-group">
-                            <label for="comment_content">Your Comment</label>
-                            <textarea class="form-control" name="comment_content" rows="3"></textarea>
-                        </div>
-                        <button type="submit" class="btn btn-primary" name="create_comment">Submit</button>
-                    </form>
-                </div>
-
-                <hr>
-
-                <!-- Posted Comments -->
-                <?php
-                    if (isset($_GET['p_id'])) {
-                        $post_id = $_GET['p_id'];
-
-                        $query = "SELECT * FROM comments WHERE comment_post_id = {$post_id} AND comment_status = 'approved' ORDER BY comment_id DESC";
-                        $all_post_comments_query = mysqli_query($db_connection, $query);
-
-                        while ($row = mysqli_fetch_assoc($all_post_comments_query)) {
-                            $comment_content = $row['comment_content'];
-                            $comment_date = $row['comment_date'];
-                            $comment_author = $row['comment_author'];
-                ?>
-
-                        <div class="media">
-                                <a class="pull-left" href="#">
-                                    <img class="media-object" src="http://placehold.it/64x64" alt="">
-                                </a>
-                                <div class="media-body">
-                                    <h4 class="media-heading"><?php echo $comment_author;?>
-                                        <small><?php echo $comment_date;?></small>
-                                    </h4>
-                                    <?php echo $comment_content;?>
-                                </div>
-                            </div>
-                <?php }} ?>
-                        </div>
+              </div>
             <!-- Blog Sidebar Widgets Column -->
 <?php include "includes/sidebar.php";?>
         </div>
